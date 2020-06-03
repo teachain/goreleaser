727a6725377e6338ac6f328b024ae49752a209d5
teachain <281477138@qq.com>
2020-06-03 14:10:04 +0800

    test .travis.yaml

---
+++ event/base.go
@@ -0,0 +1,7 @@
+package event
+
+import "fmt"
+
+func Echo(text string){
+	fmt.Println("echo ",text)
+}
---
+++ event/base_test.go
@@ -0,0 +1,7 @@
+package event
+
+import "testing"
+
+func TestEcho(t *testing.T) {
+	Echo("damin")
+}
